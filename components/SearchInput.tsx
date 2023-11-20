082f8c6e834c9e686ab66bace3376099d972067a
taipericardo <ricardo.taipe@devsu.com>
2023-11-20 00:50:03 -0500

    add serach input for mobile

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function SearchInput() {
-  return <div>SearchInput</div>;
+  return (
+    <div className="relative">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-16 pointer-events-none">
+        <svg
+          className="w-6 h-6 text-accent-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 20 20"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+          />
+        </svg>
+      </div>
+      <input
+        type="text"
+        name="search"
+        className="shadow-lg text-accent-1 placeholder:text-accent-2 text-2xl rounded-lg block w-full ps-36 p-10 font-light bg-primary-1 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        placeholder="Search for a country..."
+      />
+    </div>
+  );
 }
 
 export default SearchInput;
