8599053a775655e1d59afbc6164f33c1e023bc14
taipericardo <ricardo.taipe@devsu.com>
2023-11-24 00:44:59 -0500

    Fix image

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -5,17 +5,17 @@ import { Country } from "@/pages";
 function CountryCard({ country }: { country: Country }) {
   return (
     <div className="shadow-md rounded-md text-xs overflow-hidden">
-      <Image
-        src={country.flags.svg}
-        sizes="100vw"
-        style={{
-          width: "100%",
-          height: "auto",
-        }}
-        width={260}
-        height={160}
-        alt={country.flags.alt}
-      />
+      <div style={{ position: "relative", width: "290px", height: "160px" }}>
+        <Image
+          alt={country.flags.alt}
+          src={country.flags.svg}
+          sizes="290px"
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
       <div className="px-6 pt-6 pb-12 bg-primary-1">
         <p className="font-bold text-lg">{country.name.common}</p>
         <p className="pt-4">
@@ -23,7 +23,8 @@ function CountryCard({ country }: { country: Country }) {
           {country.population}
         </p>
         <p className="py-2">
-          <span className="font-semibold">Region: </span>Europe
+          <span className="font-semibold">Region: </span>
+          {country.region}
         </p>
         <p>
           <span className="font-semibold">Capital: </span>
