88dbd8009b95dd970a22a7098992dedee2ef72d1
taipericardo <ricardo.taipe@devsu.com>
2023-11-23 19:29:53 -0500

    add custom input to mobile

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function SearchInput() {
   return (
     <div className="relative md:w-[500px]">
-      <div className="absolute inset-y-0 start-0 flex items-center ps-16 md:ps-8 pointer-events-none">
+      <div className="absolute inset-y-0 start-0 flex items-center ps-8 md:ps-8 pointer-events-none">
         <svg
-          className="w-6 h-6 text-accent-2"
+          className="w-4 h-4 text-accent-2"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -23,7 +23,7 @@ function SearchInput() {
       <input
         type="text"
         name="search"
-        className="shadow-lg text-accent-1 placeholder:text-accent-2 text-2xl rounded-lg block w-full py-9 md:py-6 px-32 md:px-20 md:text-base font-light bg-primary-1 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        className="shadow-lg text-accent-1 placeholder:text-accent-2 text-sm rounded-lg block w-full py-2 md:py-6 px-16 md:px-20 md:text-base font-light bg-primary-1 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
         placeholder="Search for a country..."
       />
     </div>
