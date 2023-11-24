67775b1a716dec7d646669c126482d09f87798e9
taipericardo <ricardo.taipe@devsu.com>
2023-11-23 19:22:02 -0500

    add dropdown component

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Dropdown() {
-  return <div>Dropdown</div>;
+  return (
+    <div>
+      <button
+        className="text-accent-1 bg-primary-1 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-4 text-center inline-flex items-center justify-between"
+        type="button"
+      >
+        <span>Filter by Region</span>
+        <svg
+          className="w-2.5 h-2.5 ms-3"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 10 6"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 4 4 4-4"
+          />
+        </svg>
+      </button>
+
+      <div className="">
+        <div>Africa</div>
+        <div>America</div>
+        <div>Asia</div>
+        <div>Europe</div>
+        <div>Oceania</div>
+      </div>
+    </div>
+  );
 }
 
 export default Dropdown;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,17 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import SearchInput from "@/components/SearchInput";
+import Dropdown from "@/components/Dropdown";
+import CountryList from "@/components/CountryList";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    ></main>
+    <div className="w-11/12 max-w-7xl mx-auto min-h-screen mt-5 border-2 border-red-600">
+      <div className="flex flex-col md:flex-row md:justify-between gap-16">
+        <SearchInput />
+        <Dropdown />
+      </div>
+      <CountryList />
+    </div>
   );
 }
